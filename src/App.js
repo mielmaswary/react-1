57b443a4a836ff1858d3ada4131cc2bbfdccef7b
Miel maswary <73788959+mielmaswary@users.noreply.github.com>
2022-06-28 20:42:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import TravelingCard from "./components/TravelingCard";
+import data from "./data";
+import Header from "./components/Header";
 
 function App() {
+  const travelingCards = data.map((item) => (
+    <TravelingCard id={item.id} item={item} />
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {travelingCards}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-logo"></div>
+      <div className="header-title">my travel journal.</div>
+    </div>
+  );
+};
+
+export default Header;
